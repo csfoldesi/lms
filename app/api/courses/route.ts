40dc8e7e67eb131ebf0189cebb90260d0f33e360
Csaba Földesi <csabus@gmail.com>
2025-02-02 11:44:50 +0100

    Create Course API implemented

---
+++ app/api/courses/route.ts
@@ -0,0 +1,25 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  try {
+    const { userId } = await auth();
+    const { title } = await request.json();
+
+    if (!userId) {
+      return new NextResponse("Unathorized", { status: 401 });
+    }
+
+    const course = await db.course.create({
+      data: {
+        userId,
+        title,
+      },
+    });
+    return NextResponse.json(course);
+  } catch (error) {
+    console.log("[COURSES]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}

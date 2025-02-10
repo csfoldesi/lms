7a779cd92460af253a1be95417f317b365e8636c
Csaba Földesi <csabus@gmail.com>
2025-02-10 23:13:35 +0100

    Chapter access setting implemented

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,11 +1,12 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
-import { ArrowLeft, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
+import { ChapterAccessForm } from "./_components/chapter-access-form";
 
 const ChapterIdPage = async ({ params }: { params: { courseId: string; chapterId: string } }) => {
   const { userId } = await auth();
@@ -59,6 +60,13 @@ const ChapterIdPage = async ({ params }: { params: { courseId: string; chapterId
           </div>
           <ChapterTitleForm initialData={chapter} courseId={courseId} chapterId={chapterId} />
           <ChapterDescriptionForm initialData={chapter} courseId={courseId} chapterId={chapterId} />
+        </div>
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={Eye} />
+            <h2 className="text-xl">Access Settings</h2>
+          </div>
+          <ChapterAccessForm initialData={chapter} courseId={courseId} chapterId={chapterId} />
         </div>
       </div>
     </div>

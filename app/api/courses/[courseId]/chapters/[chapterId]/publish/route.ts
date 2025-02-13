417c5322a375bd05fb3b0423dab29f82db3ce2c4
Csaba Földesi <csabus@gmail.com>
2025-02-13 21:50:44 +0100

    Publish/Unpublish chapter implemented

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/publish/route.ts
@@ -0,0 +1,53 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(request: Request, { params }: { params: { courseId: string; chapterId: string } }) {
+  try {
+    const { userId } = await auth();
+    const { courseId, chapterId } = await params;
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+    });
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const chapter = await db.chapter.findUnique({
+      where: {
+        id: chapterId,
+        courseId: courseId,
+      },
+    });
+    const muxData = await db.muxData.findFirst({
+      where: {
+        chapterId: chapterId,
+      },
+    });
+    if (!chapter || !muxData || !chapter.title || !chapter.description || !chapter.videoUrl) {
+      return new NextResponse("Missing required fields", { status: 400 });
+    }
+
+    const publishedChapter = await db.chapter.update({
+      where: {
+        id: chapterId,
+        courseId: courseId,
+      },
+      data: {
+        isPublished: true,
+      },
+    });
+
+    return NextResponse.json(publishedChapter);
+  } catch (error) {
+    console.log("CHAPTER_PUBLISH", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
---
+++ app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -0,0 +1,56 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function PATCH(request: Request, { params }: { params: { courseId: string; chapterId: string } }) {
+  try {
+    const { userId } = await auth();
+    const { courseId, chapterId } = await params;
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId: userId,
+      },
+    });
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const unPublishedChapter = await db.chapter.update({
+      where: {
+        id: chapterId,
+        courseId: courseId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    // if no published chapters in course, unpublish course
+    const publishedChaptersInCourse = await db.chapter.findMany({
+      where: {
+        courseId: courseId,
+        isPublished: true,
+      },
+    });
+    if (!publishedChaptersInCourse.length) {
+      await db.course.update({
+        where: {
+          id: courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+
+    return NextResponse.json(unPublishedChapter);
+  } catch (error) {
+    console.log("CHAPTER_UNPUBLISH", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}

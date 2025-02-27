d9b332a24a661659ec0ef143ab961648119754f7
Csaba Földesi <csabus@gmail.com>
2025-02-27 18:15:22 +0100

    COurse search fix

--- actions/get-courses.ts
+++ actions/get-courses.ts
@@ -25,6 +25,7 @@ export const getCourses = async ({
         isPublished: true,
         title: {
           contains: title,
+          mode: "insensitive",
         },
         categoryId: categoryId,
       },

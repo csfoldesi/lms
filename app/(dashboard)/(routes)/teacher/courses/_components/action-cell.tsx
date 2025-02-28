7965bed64c5ac098f38a7153cc2ab2b11f89c609
Csaba Földesi <csabus@gmail.com>
2025-02-28 22:22:14 +0100

    Delete course omplemented on teacher/course page

--- app/(dashboard)/(routes)/teacher/courses/_components/action-cell.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/action-cell.tsx
@@ -1,58 +1,58 @@
+import { Hint } from "@/components/hint";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { MoreHorizontal, Pencil, Trash } from "lucide-react";
+import { useConfirm } from "@/hooks/use-confirm";
+import axios from "axios";
+import { Pencil, Trash } from "lucide-react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface ActionCellProps {
   id: string;
 }
 
 export function ActionCell({ id }: ActionCellProps) {
-  const [isLoading, setIsLoading] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [ConfirmDialog, confirm] = useConfirm(
+    "Delete Course",
+    "Are you sure you want to delete this course? This cannot be undone."
+  );
+  const router = useRouter();
+
   const onDelete = async () => {
-    /*setIsLoading(true);
-        try {
-          await axios.delete(`/api/courses/${courseId}`);
-          toast.success("Course deleted");
-          router.refresh();
-          router.push(`/teacher/courses`);
-        } catch {
-          toast.error("Something went wrong");
-        } finally {
-          setIsLoading(false);
-        }*/
+    const ok = await confirm();
+    if (!ok) return;
+    console.log("deleting");
+    setIsLoading(true);
+    try {
+      await axios.delete(`/api/courses/${id}`);
+      toast.success("Course deleted");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-4 w-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <MoreHorizontal className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem>
-            <Link href={`/teacher/courses/${id}`} className="flex items-center gap-x-2 w-full">
-              <Pencil className="h-4 w-4 mr-2" />
-              Edit
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="text-destructive">
-            <Button variant="ghost" onClick={onDelete}>
-              <Trash className="h-4 w-4 mr-2" />
-              <span>Delete</span>
+      <ConfirmDialog />
+      <div className="flex items-center justify-end gap-x-2 w-full">
+        <Link href={`/teacher/courses/${id}`}>
+          <Hint label="Edit course">
+            <Button variant="ghost">
+              <Pencil className="h-4 w-4" />
             </Button>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+          </Hint>
+        </Link>
+        <Hint label="Delete course">
+          <Button variant="ghost" onClick={onDelete} title="Delete course" disabled={isLoading}>
+            <Trash className="h-4 w-4 text-destructive" />
+          </Button>
+        </Hint>
+      </div>
     </>
   );
 }
